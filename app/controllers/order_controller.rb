202e84f2b49d8932c21f00f980ad1037eaaaf8d2
DOJO <kevin11272@gmail.com>
2024-01-23 13:59:30 +0800

    feat: create user login logout signup

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,27 @@
+class OrderController < ApplicationController
+    def index
+      
+    end
+
+    def new
+      
+    end
+
+    def edit
+      
+    end
+
+    def updata
+      
+    end
+
+    def destroy
+      
+    end
+
+    private
+
+    def order_params
+      
+    end
+end
